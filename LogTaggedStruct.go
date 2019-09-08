fe146de5028ea52e62150ea731c7936d8d8bd4c6
Daniel Unverricht <daniel.unverricht@pentlandfirth.com>
2019-09-08 12:31:46 +0200

    add prefix

--- LogTaggedStruct.go
+++ LogTaggedStruct.go
@@ -1,20 +1,21 @@
 package ulog
 
 import (
+	"fmt"
 	"reflect"
 )
 
-func LogEnvStruct(envStruct interface{}) {
-	LogTaggedStruct(envStruct, "env")
+func LogEnvStruct(envStruct interface{}, prefix string) {
+	LogTaggedStruct(envStruct, "env", prefix)
 }
 
-func LogTaggedStruct(taggedStruct interface{}, tag string) {
+func LogTaggedStruct(taggedStruct interface{}, tag string, prefix string) {
 	typeOf := reflect.TypeOf(taggedStruct)
 	valueOf := reflect.ValueOf(taggedStruct)
 	for i := 0; i < typeOf.NumField(); i++ {
 		tag := typeOf.Field(i).Tag.Get(tag)
 		if tag != "" {
-			Infof("%s = %v", tag, valueOf.Field(i))
+			Infof(fmt.Sprintf("%s", prefix)+"%s = %v", tag, valueOf.Field(i))
 		}
 	}
 }
