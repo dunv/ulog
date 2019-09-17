2545bf36fe6914a58a298067d618d58ab753d067
Daniel <daniel.unverricht@pentlandfirth.com>
2019-09-17 15:04:45 +0200

    add more methods to logger

--- ULogger.go
+++ ULogger.go
@@ -15,24 +15,34 @@ type ULogger interface {
 	Panicf(fmtString string, v ...interface{})
 	Fatal(v ...interface{})
 	Fatalf(fmtString string, v ...interface{})
+	LogIfError(err error)
+	LogIfErrorSecondArg(tmp interface{}, err error)
+	LogIfErrorToInfo(err error)
+	LogIfErrorToInfoSecondArg(tmp interface{}, err error)
 }
 
 type ULog struct{}
 
-func (l ULog) Trace(v ...interface{})                    { Trace(v...) }
-func (l ULog) Tracef(fmtString string, v ...interface{}) { Tracef(fmtString, v...) }
-func (l ULog) Debug(v ...interface{})                    { Debug(v...) }
-func (l ULog) Debugf(fmtString string, v ...interface{}) { Debugf(fmtString, v...) }
-func (l ULog) Info(v ...interface{})                     { Info(v...) }
-func (l ULog) Infof(fmtString string, v ...interface{})  { Infof(fmtString, v...) }
-func (l ULog) Warn(v ...interface{})                     { Warn(v...) }
-func (l ULog) Warnf(fmtString string, v ...interface{})  { Warnf(fmtString, v...) }
-func (l ULog) Error(v ...interface{})                    { Error(v...) }
-func (l ULog) Errorf(fmtString string, v ...interface{}) { Errorf(fmtString, v...) }
-func (l ULog) Panic(v ...interface{})                    { Panic(v...) }
-func (l ULog) Panicf(fmtString string, v ...interface{}) { Panicf(fmtString, v...) }
-func (l ULog) Fatal(v ...interface{})                    { Fatal(v...) }
-func (l ULog) Fatalf(fmtString string, v ...interface{}) { Fatalf(fmtString, v...) }
+func (l ULog) Trace(v ...interface{})                         { Trace(v...) }
+func (l ULog) Tracef(fmtString string, v ...interface{})      { Tracef(fmtString, v...) }
+func (l ULog) Debug(v ...interface{})                         { Debug(v...) }
+func (l ULog) Debugf(fmtString string, v ...interface{})      { Debugf(fmtString, v...) }
+func (l ULog) Info(v ...interface{})                          { Info(v...) }
+func (l ULog) Infof(fmtString string, v ...interface{})       { Infof(fmtString, v...) }
+func (l ULog) Warn(v ...interface{})                          { Warn(v...) }
+func (l ULog) Warnf(fmtString string, v ...interface{})       { Warnf(fmtString, v...) }
+func (l ULog) Error(v ...interface{})                         { Error(v...) }
+func (l ULog) Errorf(fmtString string, v ...interface{})      { Errorf(fmtString, v...) }
+func (l ULog) Panic(v ...interface{})                         { Panic(v...) }
+func (l ULog) Panicf(fmtString string, v ...interface{})      { Panicf(fmtString, v...) }
+func (l ULog) Fatal(v ...interface{})                         { Fatal(v...) }
+func (l ULog) Fatalf(fmtString string, v ...interface{})      { Fatalf(fmtString, v...) }
+func (l ULog) LogIfError(err error)                           { LogIfError(err) }
+func (l ULog) LogIfErrorSecondArg(tmp interface{}, err error) { LogIfErrorSecondArg(tmp, err) }
+func (l ULog) LogIfErrorToInfo(err error)                     { LogIfErrorToInfo(err) }
+func (l ULog) LogIfErrorToInfoSecondArg(tmp interface{}, err error) {
+	LogIfErrorToInfoSecondArg(tmp, err)
+}
 
 func NewUlog() *ULog {
 	return &ULog{}
