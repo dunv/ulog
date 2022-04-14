03244ca2eb67a09fd4a59bc5b30bb15abb15f737
Daniel Unverricht <daniel.unverricht@pentlandfirth.com>
2022-04-14 12:15:25 +0200

    update skipfunctions

--- SkipFunctions.go
+++ SkipFunctions.go
@@ -8,8 +8,14 @@ import (
 
 var origSkipFunctions = []string{
 	"github.com/dunv/ulog.LogIfError",
-	"github.com/dunv/ulog.LogIfErrorSecondArg",
+	"github.com/dunv/ulog.TraceIfError",
+	"github.com/dunv/ulog.DebugIfError",
+	"github.com/dunv/ulog.InfoIfError",
+	"github.com/dunv/ulog.WarnIfError",
+	"github.com/dunv/ulog.FatalIfError",
 	"github.com/dunv/ulog.PanicIfError",
+	"github.com/dunv/ulog.LogIfErrorSecondArg",
+	"github.com/dunv/ulog.FatalIfErrorSecondArg",
 	"github.com/dunv/ulog.PanicIfErrorSecondArg",
 	"github.com/dunv/ulog.LogIfErrorToInfo",
 	"github.com/dunv/ulog.LogIfErrorToInfoSecondArg",
@@ -50,48 +56,10 @@ var origSkipFunctions = []string{
 	"github.com/dunv/ulog.LogEnvStruct",
 }
 
-var skipFunctions = []string{
-	"github.com/dunv/ulog.LogIfError",
-	"github.com/dunv/ulog.LogIfErrorSecondArg",
-	"github.com/dunv/ulog.PanicIfError",
-	"github.com/dunv/ulog.PanicIfErrorSecondArg",
-	"github.com/dunv/ulog.LogIfErrorToInfo",
-	"github.com/dunv/ulog.LogIfErrorToInfoSecondArg",
-	"github.com/dunv/ulog.Trace",
-	"github.com/dunv/ulog.Tracef",
-	"github.com/dunv/ulog.Debug",
-	"github.com/dunv/ulog.Debugf",
-	"github.com/dunv/ulog.Info",
-	"github.com/dunv/ulog.Infof",
-	"github.com/dunv/ulog.Warn",
-	"github.com/dunv/ulog.Warnf",
-	"github.com/dunv/ulog.Error",
-	"github.com/dunv/ulog.Errorf",
-	"github.com/dunv/ulog.Panic",
-	"github.com/dunv/ulog.Panicf",
-	"github.com/dunv/ulog.Fatal",
-	"github.com/dunv/ulog.Fatalf",
-	"github.com/dunv/ulog.LogWithLevel",
-	"github.com/dunv/ulog.LogWithLevelf",
-	"github.com/dunv/ulog.ULog.Trace",
-	"github.com/dunv/ulog.ULog.Tracef",
-	"github.com/dunv/ulog.ULog.Debug",
-	"github.com/dunv/ulog.ULog.Debugf",
-	"github.com/dunv/ulog.ULog.Info",
-	"github.com/dunv/ulog.ULog.Infof",
-	"github.com/dunv/ulog.ULog.Warn",
-	"github.com/dunv/ulog.ULog.Warnf",
-	"github.com/dunv/ulog.ULog.Error",
-	"github.com/dunv/ulog.ULog.Errorf",
-	"github.com/dunv/ulog.ULog.Panic",
-	"github.com/dunv/ulog.ULog.Panicf",
-	"github.com/dunv/ulog.ULog.Fatal",
-	"github.com/dunv/ulog.ULog.Fatalf",
-	"github.com/dunv/ulog.ULog.LogWithLevel",
-	"github.com/dunv/ulog.ULog.LogWithLevelf",
-	"github.com/dunv/ulog.LogTaggedStruct",
-	"github.com/dunv/ulog.logTaggedStructWithMaskingAndWarning",
-	"github.com/dunv/ulog.LogEnvStruct",
+var skipFunctions = []string{}
+
+func init() {
+	ResetSkipFunctions()
 }
 
 func ResetSkipFunctions() {
